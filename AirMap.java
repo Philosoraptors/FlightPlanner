ad02e78dfbccfc7c8791f503e6cff8e85d3aeddf
thebareju <noahbweiner@gmail.com>
2015-01-16 16:33:54 -0800

    Added getX() and getY() to AirMap
    getX(float lng) takes a longitude and returns the corresponding
    x-coordinate, getY takes a latitude and returns a y-coordinate.
    
    I modified paintCompenent to use these.

--- AirMap.java
+++ AirMap.java
@@ -8,6 +8,41 @@ public class AirMap extends JPanel{
   private static final int PADDING = 30;
 
   private static float minLng, maxLng, minLat, maxLat;
+  
+  // calculates an x coordinate from longitude
+  public int getX(float lng) {
+    float scale;
+    int xMin;
+    float latRange = maxLat - minLat;
+    float lngRange = maxLng - minLng;
+    
+    if (latRange > lngRange) {
+      scale = (this.getHeight() - PADDING * 2) / latRange;
+      xMin = (int)((getWidth() - lngRange * scale) / 2);
+    } else {
+      scale = (getWidth() - PADDING * 2) / lngRange;
+      xMin = PADDING;
+    }
+    
+    int x = (int)((lng - minLng) * scale) + xMin;
+    return x;
+  }
+  
+  public int getY(float lat) {
+    float scale;
+    int yMin;
+    float latRange = maxLat - minLat;
+    float lngRange = maxLng - minLng;
+    if (latRange > lngRange) {
+      scale = (getHeight() - PADDING * 2) / latRange;
+      yMin = PADDING;
+    } else {
+      scale = (getWidth() - PADDING * 2) / lngRange;
+      yMin = (int)((getHeight() - latRange * scale) / 2);
+    }
+    
+    int y = -(int)((airport.lat() - minLat) * scale) + getHeight() - yMin;
+  }
 
   public static void showMap(){
     JFrame frame = new JFrame("flight map");
@@ -33,24 +68,10 @@ public class AirMap extends JPanel{
   public void paintComponent(Graphics gr){
     super.paintComponent(gr);
     
-    float scale;
-    int yMin, xMin;
-    float latRange = maxLat - minLat;
-    float lngRange = maxLng - minLng;
-    if (latRange > lngRange) {
-      scale = (getHeight() - PADDING * 2) / latRange;
-      xMin = (int)((getWidth() - lngRange * scale) / 2);
-      yMin = PADDING;
-    } else {
-      scale = (getWidth() - PADDING * 2) / lngRange;
-      yMin = (int)((getHeight() - latRange * scale) / 2);
-      xMin = PADDING;
-    }
-    
     for (int i = 0; i < Airport.Airports.size(); i++) {
       Airport airport = Airport.Airports.get(i);
-      int y = -(int)((airport.lat() - minLat) * scale) + getHeight() - yMin;
-      int x = (int)((airport.lng() - minLng) * scale) + xMin;
+      int y = getY(airport.lat());
+      int x = getX(airport.lng());
       gr.drawString(airport.name(), x, y);
     }
   }
