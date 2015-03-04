2e6e896384d0d969c1301e8191f354719fe7d92a
thebareju <noahbweiner@gmail.com>
2015-03-04 10:24:45 -0800

    Create AirMapInterface

---
+++ AirMapInterface.java
@@ -0,0 +1,11 @@
+/**
+ * AirMapInterface
+ *
+ * Contains buttons/text boxes to interact with AirMap.  The idea
+ * is to have two panels within our frame, this being one of them,
+ * the AirMap being the other.  -Noah
+ **/
+
+public class AirMapInterface extends JPanel {
+
+}
