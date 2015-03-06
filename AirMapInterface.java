6e34bc5bac5c642422fcab3632c1bd9f5e843108
Michelle Wallace <voirdire96@gmail.com>
2015-03-05 21:46:08 -0800

    Update AirMapInterface.java

--- AirMapInterface.java
+++ AirMapInterface.java
@@ -20,44 +20,69 @@ public class AirMapInterface extends JPanel implements ActionListener {
   public static final int FROM = 2;
   public int mode;
   
-     
-  JButton fromButton = new JButton("Departure");
-  JButton toButton = new JButton("Destination");
-  //JTextField text = new JTextField(5);
- // JLabel label = new JLabel("name"); 
-  
   public AirMapInterface() { 
-     setPreferredSize(new Dimension(550, 500));
-     this.add(fromButton);
-     fromButton.addActionListener(this);
-     this.add(toButton);
-     toButton.addActionListener(this);
+      setPreferredSize(new Dimension(600, 600));
+    //TravelMap();
+     
   }
   
  public void actionPerformed(ActionEvent e){
-   //if (e.getSource() == fromButton)
-   //repaint();
+   //if (e.getSource() == from)
+   repaint();
    
   }
- 
      
   public static void TravelMap(){
+     
       JFrame frame = new JFrame("Travel Map");
       frame.setVisible(true);
-      Container cnt = frame.getContentPane();
-      AirMapInterface pnl = new AirMapInterface();
-      cnt.add(pnl);
-      frame.pack();
+      frame.setSize(600, 600);
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      
+      JPanel buttonPanel = new JPanel(new GridBagLayout()); //make the panel for the buttons
+      JPanel mapPanel = new JPanel(); // make the map panel
+      
+      mapPanel.setPreferredSize(new Dimension(500, 500));
+      
+      JButton fromButton = new JButton("Departure");
+      JButton toButton = new JButton("Destination");
+      
+      GridBagConstraints layout = new GridBagConstraints();
+      
+      layout.gridx = 0;
+      layout.gridy = 1;
+      buttonPanel.add(fromButton, layout);
+      //fromButton.addActionListener(this);
+      layout.gridx = 0;
+      layout.gridy = 2;
+      buttonPanel.add(toButton, layout);
+     
+      //toButton.addActionListener(this);
+
+      // create new AirMap and display it
+      AirMap funMap = new AirMap(500,500,30);
+      
+      mapPanel.add(funMap);
+      
+      frame.add(buttonPanel, BorderLayout.EAST); // put the button panel on the right
+      frame.add(mapPanel, BorderLayout.WEST); // put the map panel on the left
+      
+      
      
   }
       
      public void paintComponent(Graphics g) {
       super.paintComponent(g);
+ 
 
 }
      
      public static void main (String[] args){
-       
+      
+// load airports and flights
+      Airport.loadAirports();
+      Flight.loadFlights();
+      
       TravelMap();
        
      }
