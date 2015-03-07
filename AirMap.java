7e6c8ecef5f95319f83adf432e7ee8c16fa76837
UserJeff <jeff.ladish@gmail.com>
2015-03-06 16:45:08 -0800

    Fixed spacing

--- AirMap.java
+++ AirMap.java
@@ -5,32 +5,32 @@ import javax.swing.*;
 
 public class AirMap extends JPanel implements MouseListener {
     
-    public int width;
-    public int height;
-    private int padding;
-    private int xcord;
-    private int ycord;
-    private boolean clickBool = false;
-    private String portClick = "";
-    private int proximity = 10;
+  public int width;
+  public int height;
+  private int padding;
+  private int xcord;
+  private int ycord;
+  private boolean clickBool = false;
+  private String portClick = "";
+  private int proximity = 10;
 
-    private static float minLng, maxLng, minLat, maxLat;
+  private static float minLng, maxLng, minLat, maxLat;
     
-    // CONSTRUCTOR
+  // CONSTRUCTOR
     
-    public AirMap(int wid, int hei, int pad) {
-	super();
+  public AirMap(int wid, int hei, int pad) {
+	  super();
 	
-	width = wid;
-	height = hei;
-	padding = pad;
+	  width = wid;
+	  height = hei;
+	  padding = pad;
 	
-	// calculate minimum and maximum latitude/longitude
-	minLng = maxLng = Airport.Airports.get(0).lng();  // need to initialize variables
-	minLat = maxLat = Airport.Airports.get(0).lat();  // in order to compare them!
+	  // calculate minimum and maximum latitude/longitude
+	  minLng = maxLng = Airport.Airports.get(0).lng();  // need to initialize variables
+	  minLat = maxLat = Airport.Airports.get(0).lat();  // in order to compare them!
 	
-	// cycle through airports
-	for (int i = 0; i < Airport.Airports.size(); i++) {
+	  // cycle through airports
+	  for (int i = 0; i < Airport.Airports.size(); i++) {
 	    Airport airport = Airport.Airports.get(i);
 	    
 	    // compare airport to previously stored values
@@ -38,34 +38,34 @@ public class AirMap extends JPanel implements MouseListener {
 	    if (airport.lat() < minLat) minLat = airport.lat();
 	    if (airport.lng() > maxLng) maxLng = airport.lng();
 	    if (airport.lng() < minLng) minLng = airport.lng();
-	}
-    }
+	  }
+  }
     
     // STATIC METHODS
     
-    public void showMap(){
-	JFrame frame = new JFrame("flight map");
-	setPreferredSize(new Dimension(500, 500));
-	addMouseListener(this);
-	frame.getContentPane().add(this);
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame.pack();
-	frame.setVisible(true);
-    }
+  public void showMap(){
+	  JFrame frame = new JFrame("flight map");
+	  setPreferredSize(new Dimension(500, 500));
+	  addMouseListener(this);
+	  frame.getContentPane().add(this);
+	  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	  frame.pack();
+	  frame.setVisible(true);
+  }
     
     // PUBLIC METHODS
     
     // called when window needs to be drawn
-    public void paintComponent(Graphics gr){
-	super.paintComponent(gr);
+  public void paintComponent(Graphics gr){
+	  super.paintComponent(gr);
 	
-    // update width and height
-	width = getWidth();
-	height = getHeight();
+  // update width and height
+	  width = getWidth();
+	  height = getHeight();
 	
 	// cycle through airports first to place airport names and 
 	// look to see what airport has been selected (clicked).
-	for (Airport airport : Airport.Airports) {
+	  for (Airport airport : Airport.Airports) {
 	    
 	    // get coordinates
 	    int y = getY(airport.lat());
@@ -77,46 +77,48 @@ public class AirMap extends JPanel implements MouseListener {
 	    //Get the value of the airport selected, and draw the name of the airport
 	    if ((y >= (ycord - proximity) && y <= (ycord + proximity)) && (x > (xcord - proximity * 3) && (x <= xcord))){        
 		
-		//Store the airport clicked on
-		portClick = airport.name();
+		    //Store the airport clicked on
+		    portClick = airport.name();
 		
-		//Draw the text string the color of the flight lines to be a key
-		gr.setColor(Color.red);
-		gr.drawString("All departing flights from: " + portClick,(width / 2),(height / 10));
-		gr.setColor(Color.blue);
-		gr.drawString("All arriving flights from: " + portClick,(width / 2),(height / 10 + 30));
-	    }
+		    //Draw the text string the color of the flight lines to be a key
+		    gr.setColor(Color.red);
+		    gr.drawString("All departing flights from: " + portClick,(width / 2),(height / 10));
+		    gr.setColor(Color.blue);
+		    gr.drawString("All arriving flights from: " + portClick,(width / 2),(height / 10 + 30));
+	   }
 	}
 	
 	// cycle through airports onc emore in order to draw lines
     drawAirportLines(gr);
 
-    }
+  }
 
     // MouseListener events
-    public void mousePressed(MouseEvent e) {
-	xcord = e.getX();
-	ycord = e.getY(); 
-	repaint();
-    }
-    public void mouseReleased(MouseEvent e) {}
-    public void mouseEntered(MouseEvent e) {}
-    public void mouseExited(MouseEvent e) {}
-    public void mouseClicked(MouseEvent e) {}
+  public void mousePressed(MouseEvent e) {
+	  xcord = e.getX();
+	  ycord = e.getY(); 
+	  repaint();
+  }
+
+  public void mouseReleased(MouseEvent e) {}
+  public void mouseEntered(MouseEvent e) {}
+  public void mouseExited(MouseEvent e) {}
+  public void mouseClicked(MouseEvent e) {}
     
     // PRIVATE FUNCTIONS
   
-    // calculates an x coordinate from longitude
-    private int getX(float lng) {
-	float scale;
-	int xMin;
-	float latRange = maxLat - minLat;
-	float lngRange = maxLng - minLng;
+  // calculates an x coordinate from longitude
+  private int getX(float lng) {
+	  float scale;
+	  int xMin;
+	  float latRange = maxLat - minLat;
+	  float lngRange = maxLng - minLng;
     
 	if (latRange > lngRange) {
 	    scale = (height - padding * 2) / latRange;
 	    xMin = (int)((width - lngRange * scale) / 2);
-	} else {
+	} 
+  else {
 	    scale = (width - padding * 2) / lngRange;
 	    xMin = padding;
 	}
@@ -125,24 +127,25 @@ public class AirMap extends JPanel implements MouseListener {
 	return x;
     }
     
-    // calculates a y coordinate from a latitude
-    private int getY(float lat) {
-	float scale;
-	int yMin;
-	float latRange = maxLat - minLat;
-	float lngRange = maxLng - minLng;
+  // calculates a y coordinate from a latitude
+  private int getY(float lat) {
+	  float scale;
+	  int yMin;
+	  float latRange = maxLat - minLat;
+	  float lngRange = maxLng - minLng;
 	
-	if (latRange > lngRange) {
+	  if (latRange > lngRange) {
 	    scale = (height - padding * 2) / latRange;
 	    yMin = padding;
-	} else {
+	  } 
+    else {
 	    scale = (width - padding * 2) / lngRange;
 	    yMin = (int)((height - latRange * scale) / 2);
-	}
-	
-	int y = -(int)((lat - minLat) * scale) + height - yMin;
-	return y;
-    }
+	  }
+
+	  int y = -(int)((lat - minLat) * scale) + height - yMin;
+	  return y;
+  }
 
   //STATIC FUNCTIONS
 
