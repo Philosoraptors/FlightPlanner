721d0474384ab5b1b02f1080203c4c4cae4407f6
thebareju <noahbweiner@gmail.com>
2015-03-06 16:18:54 -0800

    funMap is working

--- AirMapInterface.java
+++ AirMapInterface.java
@@ -40,9 +40,6 @@ public class AirMapInterface extends JPanel implements ActionListener {
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       
       JPanel buttonPanel = new JPanel(new GridBagLayout()); //make the panel for the buttons
-      JPanel mapPanel = new JPanel(); // make the map panel
-      
-      mapPanel.setPreferredSize(new Dimension(500, 500));
       
       JButton fromButton = new JButton("Departure");
       JButton toButton = new JButton("Destination");
@@ -61,21 +58,15 @@ public class AirMapInterface extends JPanel implements ActionListener {
 
       // create new AirMap and display it
       AirMap funMap = new AirMap(500,500,30);
+      funMap.setPreferredSize(new Dimension(500, 500));
       
-      mapPanel.add(funMap);
-      
-      frame.add(buttonPanel, BorderLayout.EAST); // put the button panel on the right
-      frame.add(mapPanel, BorderLayout.WEST); // put the map panel on the left
-      
-      
-     
+      frame.getContentPane().add(buttonPanel, BorderLayout.EAST); // put the button panel on the right
+      frame.getContentPane().add(funMap, BorderLayout.WEST); // put the map panel on the left
   }
       
      public void paintComponent(Graphics g) {
       super.paintComponent(g);
- 
-
-}
+     }
      
      public static void main (String[] args){
       
