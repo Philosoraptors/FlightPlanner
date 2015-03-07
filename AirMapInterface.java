e969314e113ec2c66be4a6babffa7659ffa31615
Michelle Wallace <voirdire96@gmail.com>
2015-03-06 17:18:02 -0800

    Added a quit button

--- AirMapInterface.java
+++ AirMapInterface.java
@@ -5,7 +5,7 @@
  * is to have two panels within our frame, this being one of them,
  * the AirMap being the other.  -Noah
  * 
- * Made the buttons to click --Michelle
+ * Made the buttons panel and added the AirMap to show next to it. --Michelle
  **/
 
 import java.awt.*;
@@ -20,15 +20,33 @@ public class AirMapInterface extends JPanel implements ActionListener {
   public static final int FROM = 2;
   public int mode;
   
+  public static JPanel buttonPanel = new JPanel(new GridBagLayout()); //make the panel for the buttons
+      
+      public static JButton fromButton = new JButton("Departure");
+      public static JButton toButton = new JButton("Destination");
+      public static JButton quitButton = new JButton("Quit");
+      
+      
+  
   public AirMapInterface() { 
-      setPreferredSize(new Dimension(600, 600));
-    //TravelMap();
+     setPreferredSize(new Dimension(600, 600));
+     //this.add(fromButton);
+     fromButton.addActionListener(this);
+     //this.add(toButton);
+     toButton.addActionListener(this);
+     this.add(quitButton);
+     quitButton.addActionListener(this);
+      
+      
      
   }
   
- public void actionPerformed(ActionEvent e){
-   //if (e.getSource() == from)
-   repaint();
+   public void actionPerformed(ActionEvent e){
+     if (e.getSource() == quitButton)
+       System.exit(0);
+    
+     //funMap.getSelected();
+     //repaint();
    
   }
      
@@ -36,37 +54,40 @@ public class AirMapInterface extends JPanel implements ActionListener {
      
       JFrame frame = new JFrame("Travel Map");
       frame.setVisible(true);
-      frame.setSize(600, 600);
+      frame.setSize(1000, 600);
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      
-      JPanel buttonPanel = new JPanel(new GridBagLayout()); //make the panel for the buttons
-      
-      JButton fromButton = new JButton("Departure");
-      JButton toButton = new JButton("Destination");
       
       GridBagConstraints layout = new GridBagConstraints();
       
       layout.gridx = 0;
       layout.gridy = 1;
       buttonPanel.add(fromButton, layout);
-      //fromButton.addActionListener(this);
+     
       layout.gridx = 0;
       layout.gridy = 2;
       buttonPanel.add(toButton, layout);
-     
-      //toButton.addActionListener(this);
-
-      // create new AirMap and display it
-      AirMap funMap = new AirMap(500,500,30);
-      funMap.setPreferredSize(new Dimension(500, 500));
       
-      frame.getContentPane().add(buttonPanel, BorderLayout.EAST); // put the button panel on the right
-      frame.getContentPane().add(funMap, BorderLayout.WEST); // put the map panel on the left
+      layout.gridx = 0;
+      layout.gridy = 3;
+      buttonPanel.add(quitButton, layout);
+      
+      
+      // create new AirMap and display it
+      AirMap funMap = new AirMap(700,500,30);
+      
+      funMap.setPreferredSize(new Dimension(800, 600));
+      
+      frame.add(buttonPanel, BorderLayout.EAST); // put the button panel on the right
+      frame.add(funMap, BorderLayout.WEST); // put the map panel on the left
+    
+     
   }
       
      public void paintComponent(Graphics g) {
       super.paintComponent(g);
-     }
+ 
+
+}
      
      public static void main (String[] args){
       
